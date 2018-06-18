6be55531b02b5082b53d0b1a02a04a284980ab11
henvisi-1994 <henvisi_1994@hotmail.com>
2018-06-18 16:32:16 -0500

    Se realizo menus y actividad de reserva Machalilla

--- app/src/main/java/com/naturalreserv/jlh/naturalreserv1/actCosta.java
+++ app/src/main/java/com/naturalreserv/jlh/naturalreserv1/actCosta.java
@@ -1,24 +1,23 @@
 package com.naturalreserv.jlh.naturalreserv1;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MenuActivity extends AppCompatActivity {
-    Button btnCosta;
-    @SuppressLint("WrongViewCast")
+public class actCosta extends AppCompatActivity {
+
+    Button btnPro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
-        btnCosta = (Button) findViewById(R.id.imgbCosta);
-        btnCosta.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_act_costa);
+        btnPro = (Button) findViewById(R.id.btnGuayas);
+        btnPro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(),actCosta.class);
+                Intent intent = new Intent(view.getContext(), actProv.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/naturalreserv/jlh/naturalreserv1/actProv.java
+++ app/src/main/java/com/naturalreserv/jlh/naturalreserv1/actProv.java
@@ -1,26 +1,25 @@
 package com.naturalreserv.jlh.naturalreserv1;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MenuActivity extends AppCompatActivity {
-    Button btnCosta;
-    @SuppressLint("WrongViewCast")
+public class actProv extends AppCompatActivity {
+Button btnMachalilla;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
-        btnCosta = (Button) findViewById(R.id.imgbCosta);
-        btnCosta.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_act_prov);
+        btnMachalilla = (Button) findViewById(R.id.btnMachalilla);
+        btnMachalilla.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(),actCosta.class);
+                Intent intent = new Intent(view.getContext(),actMachalilla.class);
                 startActivity(intent);
             }
         });
+
     }
 }
